b7b8ff2386d7de489c9e96e2fa65d1c6207f5be7
Sonya Corcoran || Sox <sonyacorcoran@gmail.com>
2018-09-26 13:44:20 +1000

    changed first example to camelCase

--- definitions/ternary.js
+++ definitions/ternary.js
@@ -1,14 +1,21 @@
-const examples = [
-  { type: 'code', content: `ifThisISTrue ? doThis : elseDothisone` },
-  { type: 'snippet', content `let ifThisIsTrue = true;
-ifThisIsTrue ? console.log('do this') : console.log('else do this one');`
-},
-  { type: 'snippet', content `let ifThisIsTrue = true;
-let ourAnswer = ifThisIsTrue ? 'answer when true' : 'answer when false';
+const examples = [{
+      type: 'code',
+      content: `ifThisIsTrue ? doThis : elseDoThisOne`
+    },
+    {
+      type: 'snippet',
+      content `let ifThisIsTrue = true; ifThisIsTrue ? console.log('do this') : console.log('else do this one');`
+    },
+    {
+      type: 'snippet',
+      content `let ifThisIsTrue = true; let ourAnswer = ifThisIsTrue ? 'answer when true' : 'answer when false';
 console.log('we got back', ourAnswer);`
-},
+  },
 ];
 
 const synonyms = [];
 
-export { examples, synonyms }
+export {
+  examples,
+  synonyms
+}
